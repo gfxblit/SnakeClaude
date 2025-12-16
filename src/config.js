b514dcf2457407c085a0d8016c1f938d4b2acbfc
Bill Chen <3580951+gfxblit@users.noreply.github.com>
2025-12-16 15:52:10 -0800

    update config for speed

--- src/config.js
+++ src/config.js
@@ -11,6 +11,6 @@ export const FRAME_RATE = 5; // frames per second
 
 
 export const SNAKE_SPEED_CELLS_PER_SECOND = 2; // Snake moves 2 grid cells per second
-export const SNAKE_SPEED_INCREMENT_INTERVAL_SCORE = 5; // Increase speed every 5 points
-export const SNAKE_SPEED_INCREMENT_AMOUNT = 0.5; // Increase speed by 0.5 cells/sec
+export const SNAKE_SPEED_INCREMENT_INTERVAL_SCORE = 1; // Increase speed every x points
+export const SNAKE_SPEED_INCREMENT_AMOUNT = 1; // Increase speed by x cells/sec
 export const SNAKE_SPEED_MAX_CELLS_PER_SECOND = 10; // Maximum snake speed
